0d5c138e6f7e233c685214e61c831a5b794be0d2
Sebastian Calderon <naoseifutebol@gmail.com>
2024-09-22 17:08:11 -0400

    Add server error splash page

--- middlewares/errorhandlers.js
+++ middlewares/errorhandlers.js
@@ -3,11 +3,11 @@ const errorHandler = (error, req, res, next) => {
   // set status code to 500
   res.status(500);
 
-  // return internal server error text
-  res.send("Internal Server Error");
-
   //console log error
   console.log(error.stack);
+  
+  // return internal server error page
+  res.render("serverError.ejs");
 };
 
 export{errorHandler};
