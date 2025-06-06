5175520f4bc3870a593ca95f73b3011ee3ef7b39
Sebastian Calderon <naoseifutebol@gmail.com>
2025-06-05 20:57:30 -0400

    Remove cron operations (moved to separate app)

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 import "dotenv/config";
-import cron from "node-cron";
 import express from "express";
 import session from "express-session";
 import mountRoutes from "./routes/index.js";
 import bodyParser from "body-parser";
 import { errorHandler } from "./middlewares/errorhandlers.js";
-import { getLatestItemData } from "./Scripts/tickerUpdate.js";
-import { openingValuePull } from "./Scripts/tickerUpdateStartofDay.js";
 import pgS from "connect-pg-simple";
 import * as db from "./db/index.js";
 import queries from "./db/queries.js";
@@ -82,20 +79,6 @@ passport.serializeUser((user, cb) => {
 
 passport.deserializeUser((user, cb) => {
   cb(null, user);
-});
-
-// update day values on first startup
-openingValuePull();
-
-// cron schedule to automatically update item values at every 5th minute from 2 through 57
-cron.schedule("2-57/5 * * * *", () => getLatestItemData(), {
-  scheduled: true,
-});
-
-// cron scheudle to automatically update opening values daily at 00:06 UTC
-cron.schedule("6 0 * * *", () => openingValuePull(), {
-  scheduled: true,
-  timezone: "Etc/UTC",
 });
 
 app.listen(port, () => {
